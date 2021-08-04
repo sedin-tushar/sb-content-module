28e490334dbefb4937899041545a1a48f91959bf
subranil <shuvranil.saha1993@gmail.com>
2021-08-04 19:08:20 +0530

    Issue #SB-21616 feat: added browse by subject in content-section module

--- projects/content-section/src/public-api.ts
+++ projects/content-section/src/public-api.ts
@@ -4,4 +4,6 @@
 
 export * from './lib/services/content-section.service';
 export * from './lib/components/content-section/content-section.component';
+export * from './lib/components/subject-section/subject-section.component';
+export * from './lib/services/subject-section.service';
 export * from './lib/content-section.module';
